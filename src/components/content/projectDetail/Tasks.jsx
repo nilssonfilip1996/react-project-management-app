05dc863d251d11ef9c14f2bb93270dda41fec2ae
nilssonfilip1996 <nilssonfilip1996@hotmail.com>
2025-06-13 12:38:27 +0200

    Made use of react-dom's createPortal to move the deleteModal jsx to the root of the DOM-tree.

--- src/components/content/projectDetail/Tasks.jsx
+++ src/components/content/projectDetail/Tasks.jsx
@@ -19,7 +19,7 @@ export default function Tasks({taskList, manageTask}) {
 
     function createTaskList() {
         return (
-            <ul className="list-disc bg-stone-200 p-4 ps-6 rounded-md">
+            <ul className="list-disc bg-stone-200 py-2 px-8 rounded-md">
                 {taskList.map((task, index) => (
                 <li key={index} className="my-1">
                     <div className="flex flex-row justify-between">
@@ -34,11 +34,11 @@ export default function Tasks({taskList, manageTask}) {
 
     return (
         <div>
-            <div className="flex flex-col">
+            <div className="flex flex-col mb-6">
                 <h2 className="font-semibold text-2xl mt-4">Tasks</h2>
-                <div className="flex flex-row gap-2 my-4">
+                <div className="flex flex-row gap-2">
                     <input className="grow bg-stone-200 rounded-md px-3.5 py-2" type="text" ref={newTaskInput}/>
-                    <button className="px-2" onClick={handleNewTask}>Add Task</button>
+                    <button className="px-2 bg-stone-400 hover:bg-stone-600 rounded-lg" onClick={handleNewTask}>Add Task</button>
                 </div>
                 <div className="text-red-400" hidden={true} ref={newTaskError}>Task can't be empty.</div>
             </div>
